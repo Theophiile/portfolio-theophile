ae0753abdb805788391cb251c11186a0e7cde119
Théophile Pinto <theo-pinto@hotmail.fr>
2025-05-03 15:31:21 +0200

    modif header pour avoir le menu depliant en responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Header.scss'
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
+
   return (
     <header className="header">
       <nav className="nav">
         <div className="logo">TP</div>
-        <ul className="nav-links">
-          <li><a href="#accueil">Accueil</a></li>
-          <li><a href="#apropos">À propos</a></li>
-          <li><a href="#projets">Projets</a></li>
-          <li><a href="#competences">Compétences</a></li>
-          <li><a href="#contact">Contact</a></li>
+        <button className="menu-toggle" onClick={toggleMenu} aria-label="Menu">
+          <span className={`hamburger ${isMenuOpen ? 'open' : ''}`}></span>
+        </button>
+        <ul className={`nav-links ${isMenuOpen ? 'open' : ''}`}>
+          <li><a href="#accueil" onClick={() => setIsMenuOpen(false)}>Accueil</a></li>
+          <li><a href="#apropos" onClick={() => setIsMenuOpen(false)}>À propos</a></li>
+          <li><a href="#projets" onClick={() => setIsMenuOpen(false)}>Projets</a></li>
+          <li><a href="#competences" onClick={() => setIsMenuOpen(false)}>Compétences</a></li>
+          <li><a href="#contact" onClick={() => setIsMenuOpen(false)}>Contact</a></li>
         </ul>
       </nav>
     </header>
